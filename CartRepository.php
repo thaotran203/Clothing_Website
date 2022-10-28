35fa9015e89e368fca6538cfd2a6db0645bdf083
nguyentranazz123 <108350830+nguyentranazz123@users.noreply.github.com>
2022-10-29 00:51:37 +0700

    Add files via upload

---
+++ CartRepository.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Cart;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use  Doctrine\ORM\Query;
+
+/**
+ * @extends ServiceEntityRepository<Cart>
+ *
+ * @method Cart|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Cart|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Cart[]    findAll()
+ * @method Cart[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CartRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Cart::class);
+    }
+
+    public function add(Cart $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Cart $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function reviewCart($user): Query
+    {
+    
+    $entityManager = $this->getEntityManager();
+    $qb = $entityManager->createQueryBuilder();
+    $qb->select( 'c')
+   ->from('App:Cart', 'c')
+   ->where('c.User= :user')
+   ->setParameter('user', $user);
+   return $qb->getQuery();
+    }
+
+
+
+
+//    /**
+//     * @return Cart[] Returns an array of Cart objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('c.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Cart
+//    {
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
